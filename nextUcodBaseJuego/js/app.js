56c3ec2ce49b40a76f3cc2c6b08981bdd2c3861a
jose villabona <jjvillabona1@gmail.com>
2019-06-15 21:41:58 -0500

    timer

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -10,6 +10,26 @@ function parpadeo(selector) {
     .animate({
         opacity: '0.50'
     },500,{
-        
-    })
+        step: function(){
+            $(this).css('color','yellow');
+        },
+        queue: true
+    },500)
+    .animate({
+        opacity: '0.50'
+    }, {
+        step: function () {
+            $(this).css('color', 'white');
+        },
+        queue: true
+    },500)
+    .animate({
+        opacity: '0.50'
+    }, {
+        step: function () {
+            $(this).css('color', 'yellow');
+            parpadeo('h1.main-titulo');
+        },
+        queue: true
+    });
 }
